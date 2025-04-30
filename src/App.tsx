5fae5d822fff89046f4338acd1d205367d8f7cf7
Eliel Corona <elielcoronam@gmail.com>
2025-04-30 16:38:01 -0600

    navbar

--- src/App.tsx
+++
@@ -1,6 +0,0 @@
-function App() {
-    //jsx
-    return <p>Qross Pass</p>;
-}
-
-export default App;
---
+++ src/components/Banners/CarruselBanners.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const CarruselBanners = () => {
+  return (
+    <div>CarruselBanners</div>
+  )
+}
